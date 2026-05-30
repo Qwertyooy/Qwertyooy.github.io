a9cb511ed8e82b53d0d830388b861d0b3e4592d0
Qwertyooy <xyznayo@gmail.com>
2026-05-30 18:52:25 +0700

    test deploy

--- Antrian-tgx/admin/main.js
+++ Antrian-tgx/admin/main.js
@@ -2,16 +2,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/fireba
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
-const firebaseConfig = {
-   apiKey: "AIzaSyBxrwtVjSiRXxuPVt9JHYxmzmPBp_lTWBs",
-  authDomain: "antrian-tgx.firebaseapp.com",
-  databaseURL: "https://antrian-tgx-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "antrian-tgx",
-  storageBucket: "antrian-tgx.firebasestorage.app",
-  messagingSenderId: "24957448394",
-  appId: "1:24957448394:web:5573dd09b9a4d52ff70433",
-  measurementId: "G-ET5326VSK9"
-};
+import firebaseConfig from "../firebase-config.js";
 
 
 const app = initializeApp(firebaseConfig);
